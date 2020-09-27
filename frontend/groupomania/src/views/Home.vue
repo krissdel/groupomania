f6f3308afce8aa767154046b0de842cb60ae8ca3
christian <delormechrist@gmail.com>
2020-09-28 01:43:26 +0200

    logo

---
+++ frontend/groupomania/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div class=logo>
+      <img alt="Vue logo" src="../assets/icon-above-font.png">
+    </div>
+    <Groupomania msg="Welcome to Groupomania"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Groupomania from '@/components/Groupomania.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Groupomania
+  }
+}
+</script>
+
+//  <style lang="scss">
+// .logo {
+//   font-size: 30px;
+//   color: black;
+// }
+// </style>
