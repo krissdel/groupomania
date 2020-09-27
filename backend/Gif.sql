bce3a782c5fa72a10ea78d815ce8adb794dc1deb
christian <delormechrist@gmail.com>
2020-09-27 23:19:12 +0200

    vue_cli

---
+++ backend/Gif.sql
@@ -0,0 +1,13 @@
+INSERT INTO Gif (categorie, titre, commentaires) VALUES
+('halloween', 'maison hante', NULL),
+('halloween', 'chat', NULL),
+('halloween', 'citrouille', NULL),
+('halloween', 'bougie', NULL),
+('halloween', 'fantome', NULL),
+('halloween', 'masque', NULL),
+('animaux', 'chien', NULL),
+('animaux', 'aigle', NULL),
+('animaux', 'chauve souris', NULL),
+('animaux', 'baleine', NULL),
+('animaux', 'canard', NULL);
+
